eb3e2e64a92f1a99952ef39f7158458e5e10d3ae
Noorulhuda Khan <hmnoorulhuda2014@gmail.com>
2023-08-15 00:06:43 +0500

    Q5 done

---
+++ app.js
@@ -0,0 +1,139 @@
+// CHAP 6 TO 9 ASSIGNMENT & PRACTICE
+
+
+// var num= 1;
+
+// console.log(num)
+
+// // var newNum= num++
+// var newNum= ++num
+
+// console.log(newNum)
+// console.log(num)
+
+// console.log(2+2+"2 plus 2 is "+2+2)
+
+
+// var name1= prompt("What is your name? ", "Ahmed")
+
+// alert(name1)
+
+// var num2=1
+// alert(num2+num2+"2"+num2)
+
+// var num2=2
+// var nt= "abc"
+
+// alert(num2+num2+nt+num2+num2)
+
+
+
+// Q2
+
+
+// var a=2;
+// b=1
+
+// var result= --a - --b + ++b + b--
+// //   1  (a is first decremented) - 0 (b is decremented and becomes 0) + 1(b is incremented and becomes 1) + 1(b remains same 1...will be incremented in next step)
+// // thus ( 1 - 0 + 1 + 1 =3)
+
+
+// // --a - --b + ++b + b--
+
+// console.log(a)
+
+// console.log(b)
+
+// console.log(result)
+
+
+
+
+
+
+// Q3
+
+// var hi= prompt("What is your name? ", "Name")
+
+// alert("Hello "+hi+" !")
+
+
+
+
+// Q4
+
+// var num1 = Number(prompt("Enter a number", "5"))
+
+// console.log(typeof num1)
+
+// var n =1
+// console.log(n * num1)
+
+// n++
+// console.log(n * num1)
+
+// n++
+// console.log(n * num1)
+
+// n++
+// console.log(n * num1)
+
+// n++
+// console.log(n * num1)
+// n++
+// console.log(n * num1)
+// n++
+// console.log(n * num1)
+// n++
+// console.log(n * num1)
+// n++
+// console.log(n * num1)
+// n++
+// console.log(n * num1)
+
+// Q5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
